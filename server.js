961916be6059d0724ef432629f8fb5f4db6566a1
pbelfort <42588868+pbelfort@users.noreply.github.com>
2020-02-09 17:15:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection({
     host:'localhost',
@@ -10,7 +10,7 @@ var mysqlConnection = mysql.createConnection({
     database: 'veiculodb'
 });
 
-var app = express()
+var app = express();
 
 function conecta(){
     mysqlConnection.connect((err)=>{
